0981f95bc117da0f5165759e3e358a9699117826
Sands Fish <sandsfish@gmail.com>
2013-02-28 03:27:10 -0500

    Adding details about specific timeout failure results; adding to & adjusting form to display input URI; adding field label; expanding input field.

--- app/models/Person.rb
+++ app/models/Person.rb
@@ -17,6 +17,10 @@ class Person
 
 	# TODO:   Move this into its own helper or gem (is there a dbpedia gem?)
 	# FIXME:  Rescue & handle "Request Timeout" situation
+	# => 2013-02-28T08:14:53+00:00 app[web.1]: RestClient::RequestTimeout (Request Timeout):
+	# => 2013-02-28T08:14:53+00:00 app[web.1]:   app/models/Person.rb:22:in `queryEndpoint'
+	# => 2013-02-28T08:14:53+00:00 app[web.1]:   app/models/Person.rb:9:in `getInfluenced'
+	# => 2013-02-28T08:14:53+00:00 app[web.1]:   app/controllers/people_controller.rb:3:in `index'
 	def self.queryEndpoint(endpoint, query, format = "xml")
 		# output options: "xml" (default), "json", "js", "n3", "csv"
 		return RestClient.post endpoint, :query => query, :output => format
