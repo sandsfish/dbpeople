b8b031f725abaaf28721e1d1d6ed831e0f8e3bf6
Sands Fish <sandsfish@gmail.com>
2013-02-28 01:21:36 -0500

    Initial commit of default Rails framework + addition of Thin web server.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DBPeople::Application.config.secret_token = 'c80d224fca5b58ae240e2e7277110f4d33a9e6e0e6cc21164910b06f8f44da446caa618290be339be6ac305b3c91d7ac889582956d2aeda7a3f0ef1d732bc286'
