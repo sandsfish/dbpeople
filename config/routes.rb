33c2d924df9f7a096cdf40182126afa52be643f0
Sands Fish <sandsfish@gmail.com>
2013-03-04 02:57:37 -0500

    Pushing CSS from HTML to stylesheet; Linking logo to root; Adding title; Switching root route to people#index and ditching index.html.

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ DBPeople::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'people#index'
 
   # See how all your routes lay out with "rake routes"
 
