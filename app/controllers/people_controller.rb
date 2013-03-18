c010ceddbfd33b7da419ee7d2b682e2c5bc34606
Sands Fish <sandsfish@gmail.com>
2013-03-18 02:01:15 -0400

    Adding counts to display of influencers and influenced.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,11 +3,17 @@ class PeopleController < ApplicationController
 		unless params[:person_uri].nil?
 			@influenced = Person.getInfluenced(params[:person_uri])
 			@influencers = Person.getInfluencersOf(params[:person_uri])
-		
+			
+			# FIXME: Do this in the model and return a hash with influenced, 
+			# influencers, and counts.  Better yet, do this all as a JSON
+			# web-service / AJAX call.
+			@forward_count = @influenced.count
+			@backward_count = @influencers.count
 
 		# Only do handling in rescue block, instead of doing these checks
 		# that are almost guaranteed to work
 		if params[:person_uri].nil? || params[:person_uri].empty?
+			# TODO: Create a Stats.increment_hitcount method to do this
 			stats = Stats.first
 			stats.hitcount = stats.hitcount + 1
 			stats.save
