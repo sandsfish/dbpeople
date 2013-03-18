9a8973495f1d8d3d3c49c3931df578dfd6ad6577
Sands Fish <sandsfish@gmail.com>
2013-03-18 01:27:56 -0400

    Don't bother to do the fetch on the homepage when there's no query.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,12 +1,18 @@
 class PeopleController < ApplicationController
 	def index
-		@influenced = Person.getInfluenced(params[:person_uri])
-		@influencers = Person.getInfluencersOf(params[:person_uri])
+		unless params[:person_uri].nil?
+			@influenced = Person.getInfluenced(params[:person_uri])
+			@influencers = Person.getInfluencersOf(params[:person_uri])
+		
 
+		# Only do handling in rescue block, instead of doing these checks
+		# that are almost guaranteed to work
 		if params[:person_uri].nil? || params[:person_uri].empty?
 			stats = Stats.first
 			stats.hitcount = stats.hitcount + 1
 			stats.save
+		end
+
 		end
 	end
 end
