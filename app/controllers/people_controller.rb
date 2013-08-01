98ae4a3e67ca23e69b5e37c9c362e04d9633953a
Sands Fish <sandsfish@gmail.com>
2013-08-01 11:34:41 -0400

    Initial work on retrieving & parsing JSON from SPARQL query.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -16,12 +16,16 @@ class PeopleController < ApplicationController
 					@influenced = Person.getInfluenced(params[:person_uri])
 					@influencers = Person.getInfluencersOf(params[:person_uri])
 
+					@influencedJSON = Person.getInfluencedJSON(params[:person_uri])
+					@influencersJSON = Person.getInfluencersOfJSON(params[:person_uri])
+
 					# FIXME: Do this in the model and return a hash with influenced, 
 					# influencers, and counts.  Better yet, do this all as a JSON
 					# web-service / AJAX call.
 					@forward_count = @influenced.count
 					@backward_count = @influencers.count
 				rescue RestClient::BadRequest
+					puts @influencedJSON
 					redirect_to '/', :notice => "DBPedia doesn't like this request.  :("
 				end
 			else
--- app/models/Person.rb
+++ app/models/Person.rb
@@ -21,6 +21,40 @@ class Person
 		influencers.split("\"").delete_if { |v| v.strip.empty? }[1,influencers.length]
 	end
 
+	def self.getInfluencedJSON(personURI, depth=1)
+		# Note: this query excludes other values in DBPedia that define influenced groups by subsetting to the dbpedia:Person class.
+		query = "SELECT ?influenced ?influencedName ?influencedBirth WHERE { 
+					<#{personURI}> <http://dbpedia.org/ontology/influenced> ?influenced . 
+					?influenced a <http://dbpedia.org/ontology/Person> . 
+					?influenced <http://xmlns.com/foaf/0.1/name> ?influencedName . 
+					?influenced <http://dbpedia.org/ontology/birthDate> ?influencedBirth .
+				}"
+		peopleInfluenced = queryEndpoint('http://dbpedia.org/sparql', query, "json")
+
+		# Extract / Transcode JSON result
+		ActiveSupport::JSON.decode(peopleInfluenced)['results']['bindings'].each do |record|
+			puts "#{record['influenced']['value']}"
+			puts "#{record['influencedName']['value']}"
+			puts "#{record['influencedBirth']['value']}"
+		end
+
+
+		puts peopleInfluenced
+	end
+
+	def self.getInfluencersOfJSON(personURI, depth=1)
+		# Note: this query excludes other values in DBPedia that define influenced groups by subsetting to the dbpedia:Person class.
+		query = "SELECT ?influencerName ?influencerBirth WHERE { ?influencer <http://dbpedia.org/ontology/influenced> <#{personURI}> . 
+					?influencer a <http://dbpedia.org/ontology/Person> . 
+					?influencer <http://xmlns.com/foaf/0.1/name> ?influencerName . 
+					?influencer <http://dbpedia.org/ontology/birthDate> ?influencerBirth .
+		}"
+		influencers = queryEndpoint('http://dbpedia.org/sparql', query, "json")
+		puts influencers
+	end
+
+
+
 	# TODO:   Move this into its own helper or gem (is there a dbpedia gem?)
 	# FIXME:  Rescue & handle "Request Timeout" situation
 	# => 2013-02-28T08:14:53+00:00 app[web.1]: RestClient::RequestTimeout (Request Timeout):
